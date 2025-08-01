e562a81fba5840b3700d663bab71218d4cc32ca4
SandraDoss <200701211@rajalakshmi.edu.in>
2025-08-01 21:44:53 +0530

    Removing duplicate rows

--- DataCleaning.sql
+++ DataCleaning.sql
@@ -1,4 +1,4 @@
-#Data Cleaning using world_layoffs dataset
+-- Data Cleaning using world_layoffs dataset
 
 SELECT * FROM layoffs;
 
@@ -11,12 +11,61 @@ SELECT * FROM layoffs_staging;
 INSERT layoffs_staging
 SELECT * FROM layoffs;
 
-# 1.Remove duplicates
+-- 1.Remove duplicates
 
+# Checking for duplicates
+SELECT *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging;
+
+# Creating a CTE 
+WITH duplicate_cte AS
+(
+SELECT *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num>1;
+
+# Creating a table layoffs_staging2 with row_num as a new column to delete the duplicate rows
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+   `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO layoffs_staging2
+SELECT *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+SELECT * FROM layoffs_staging2 WHERE row_num>1;
+
+# To delete the duplicates
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM layoffs_staging2 WHERE row_num>1;
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM layoffs_staging2;
 
 
 
 # 2. Standardize data
+
+
 
 
 
